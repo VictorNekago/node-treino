7e2aacda4bf3d4477748d8de38850720eb373b10
Victor <victorsousarocha@gmail.com>
2022-02-05 20:46:07 -0300

    Adição da logica do CRUD no banco de dados

--- index.js
+++ index.js
@@ -14,10 +14,51 @@ app.use(express.json())
 app.use(cors())
 
 
+
+
+// conectando ao banco de dados
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("sensores", "root", "12345", {
+    host: "localhost",
+    dialect: "mysql"
+})
+sequelize.authenticate().then(function () {
+    console.log("conectado com sucesso!")
+}).catch(function (erro) {
+    console.log("falha ao se conectar " + erro)
+})
+
+
+
+const sensorModel = sequelize.define('dados_sensores', {
+    Id: {
+        type: Sequelize.INTEGER
+    },
+    Nome: {
+        type: Sequelize.STRING
+    },
+    Tamanho: {
+        type: Sequelize.STRING
+    },
+    Tensao: {
+        type: Sequelize.STRING
+    },
+    Marca: {
+        type: Sequelize.STRING
+    },
+    Tipo: {
+        type: Sequelize.STRING
+    }
+}, { timestamps: false })
+
+
+
+
+
 let db = [
-    { '1': { Nome: 'Sensor 1', Tamanho: '3,3,3', Tensao: '3', Marca: 'A1', Tipo: 'temperatura' } },
-    { '2': { Nome: 'Sensor 2', Tamanho: '1,2,2', Tensao: '1,5', Marca: 'B3', Tipo: 'corrente elétrica' } },
-    { '3': { Nome: 'Sensor 3', Tamanho: '3,3,3', Tensao: '1,5', Marca: 'T10', Tipo: 'pressão' } }
+    { Nome: 'Sensor 1', Tamanho: '3,3,3', Tensao: '3', Marca: 'A1', Tipo: 'temperatura' },
+    { Nome: 'Sensor 2', Tamanho: '1,2,2', Tensao: '1,5', Marca: 'B3', Tipo: 'corrente elétrica' },
+    { Nome: 'Sensor 3', Tamanho: '3,3,3', Tensao: '1,5', Marca: 'T10', Tipo: 'pressão' }
 ]
 
 app.get('/', (req, res) => {
@@ -26,16 +67,26 @@ app.get('/', (req, res) => {
 
 
 app.post("/add", (req, res) => {
-    const body = req.body
 
+    try {
+        const body = req.body
+        const validarSensor = body.Tamanho != ('3,3,3') && body.Tamanho != ('1,2,2')
+        if (!body || validarSensor) {
+            throw ("Dados Invalidos")
+        }
 
+        sensorModel.create({
+            Nome: body.Nome,
+            Tamanho: body.Tamanho,
+            Tensao: body.Tensao,
+            Marca: body.Marca,
+            Tipo: body.Tipo
+        })
+        return res.status(200).json(body)
 
-
-    if (!body)
-        return res.status(400).end()
-
-    db.push(body)
-    return res.json(body)
+    } catch (error) {
+        return res.status(400).end(error)
+    }
 })
 
 app.delete('/:id', (req, res) => {
@@ -54,3 +105,6 @@ app.delete('/:id', (req, res) => {
 app.listen(21262, () => {
     console.log('Express started at http://localhost:21262')
 })
+
+
+
